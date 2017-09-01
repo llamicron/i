31c275376bd3179e7dc93968839dc3c7147cbe70
Luke Sweeney <luke@thesweeneys.org>
2017-09-01 16:32:08 -0500

    Temp commit

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from distutils.core import setup
+from i.VERSION import VERSION
 setup(
     name='llamicron-i',
     packages=['i'],
-    version='0.2',
+    version=VERSION,
     description='Connect to servers',
     author='Luke Sweeney',
     author_email='luke@thesweeneys.org',
@@ -10,5 +11,5 @@ setup(
     download_url='https://github.com/llamicron/i/archive/0.1.tar.gz',
     keywords=['ssh', 'server', 'connect'],
     classifiers=[],
-    scripts=["run"]
+    scripts=["i"]
 )
